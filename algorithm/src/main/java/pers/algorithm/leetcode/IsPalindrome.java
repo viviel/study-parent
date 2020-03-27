52d242eaeb9ddf2b42dc9fc7d5af02003b509802
郭世雄 <guoshixiong@zmeng123.com>
2020-03-27 19:25:36 +0800

    IsPalindrome

--- algorithm/src/main/java/pers/algorithm/leetcode/IsPalindrome.java
+++ algorithm/src/main/java/pers/algorithm/leetcode/IsPalindrome.java
@@ -1,13 +1,26 @@
 package pers.algorithm.leetcode;
 
+import java.util.Objects;
+
 class IsPalindrome {
 
     public static void main(String[] args) {
-        System.out.println(new IsPalindrome().isPalindrome(121));
+        System.out.println(new IsPalindrome().isPalindrome(1211));
     }
 
     public boolean isPalindrome(int x) {
-        return false;
+        String s = String.valueOf(x);
+        int i = 0, j = s.length() - 1;
+
+        while (i < j) {
+            if (!Objects.equals(s.charAt(i), s.charAt(j))) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+
+        return true;
     }
 
 }
