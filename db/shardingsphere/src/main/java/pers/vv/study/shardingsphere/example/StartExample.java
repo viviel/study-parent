3edc72136cf892f7e8fcf4cb91720ead81afab07
郭世雄 <guoshixiong@zmeng123.com>
2020-04-17 20:37:25 +0800

    shardingsphere test

--- db/shardingsphere/src/main/java/pers/vv/study/shardingsphere/example/StartExample.java
+++ db/shardingsphere/src/main/java/pers/vv/study/shardingsphere/example/StartExample.java
@@ -1,10 +1,16 @@
 package pers.vv.study.shardingsphere.example;
 
+import com.mysql.cj.jdbc.MysqlDataSource;
 import org.apache.shardingsphere.api.config.sharding.ShardingRuleConfiguration;
+import org.apache.shardingsphere.api.config.sharding.TableRuleConfiguration;
+import org.apache.shardingsphere.api.config.sharding.strategy.InlineShardingStrategyConfiguration;
 import org.apache.shardingsphere.shardingjdbc.api.ShardingDataSourceFactory;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
@@ -14,18 +20,46 @@ import java.util.Properties;
  */
 public class StartExample {
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws SQLException {
+        new StartExample().beginUse();
     }
 
     public void beginUse() throws SQLException {
         Map<String, DataSource> dataSourceMap = new HashMap<>();
+        MysqlDataSource mysqlDataSource = new MysqlDataSource();
+        mysqlDataSource.setUrl("jdbc:mysql://127.0.0.1:3306/test");
+        mysqlDataSource.setUser("root");
+        mysqlDataSource.setPassword("1234");
+        dataSourceMap.put("test", mysqlDataSource);
 
         ShardingRuleConfiguration shardingRuleConfig = new ShardingRuleConfiguration();
+
+        TableRuleConfiguration userTableRuleConfig =new TableRuleConfiguration(
+                "user", "test.user${0..1}"
+        );
+        userTableRuleConfig.setTableShardingStrategyConfig(new InlineShardingStrategyConfiguration("id", "{id % 2}"));
+        shardingRuleConfig.getTableRuleConfigs().add(userTableRuleConfig);
 
         Properties props = new Properties();
 
         DataSource dataSource = ShardingDataSourceFactory.createDataSource(dataSourceMap, shardingRuleConfig, props);
+
+        Connection connection = dataSource.getConnection();
+
+        insert(connection);
+    }
+
+    private void insert(Connection connection) throws SQLException {
+        Statement stmt = connection.createStatement();
+        stmt.execute("insert into user0(name) values ('vv')");
+    }
+
+    private void select(Connection connection) throws SQLException {
+        Statement stmt = connection.createStatement();
+        ResultSet rs = stmt.executeQuery("SELECT * FROM user0");
+        while (rs.next()) {
+            System.out.println(rs.getInt("id") + " - " + rs.getString("name"));
+        }
     }
 
 }
