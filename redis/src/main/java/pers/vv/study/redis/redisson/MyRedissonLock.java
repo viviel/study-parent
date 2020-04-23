2ec158e7dab764d14a3c60dfc96f89a2f645667a
郭世雄 <guoshixiong@zmeng123.com>
2020-04-23 20:27:04 +0800

    redisson锁

--- redis/src/main/java/pers/vv/study/redis/redisson/MyRedissonLock.java
+++ redis/src/main/java/pers/vv/study/redis/redisson/MyRedissonLock.java
@@ -4,21 +4,30 @@ import org.redisson.Redisson;
 import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * @author guoshixiong
  */
 public class MyRedissonLock {
 
+    private final RedissonClient redissonClient = Redisson.create();
+
     public static void main(String[] args) {
+        MyRedissonLock myRedissonLock = new MyRedissonLock();
+
+        new Thread(myRedissonLock::test1).start();
+        new Thread(myRedissonLock::test1).start();
     }
 
     private void test1() {
-        RedissonClient redissonClient = Redisson.create();
-
-        RLock lock = redissonClient.getLock("");
+        RLock lock = redissonClient.getLock("myLock");
 
         try {
             lock.lock();
+            TimeUnit.SECONDS.sleep(Integer.MAX_VALUE);
+        } catch (InterruptedException e) {
+            System.out.println("线程中断");
         } finally {
             lock.unlock();
         }
