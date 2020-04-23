010c6d1cb67936cc850853de56da85b9e8ecc468
郭世雄 <guoshixiong@zmeng123.com>
2020-04-23 20:31:41 +0800

    latch study

---
+++ jdk/concurrent/src/main/java/pers/vv/study/jdk/concurrent/LatchStudy.java
@@ -0,0 +1,22 @@
+package pers.vv.study.jdk.concurrent;
+
+import java.util.concurrent.CountDownLatch;
+
+public class LatchStudy {
+
+    public static void main(String[] args) {
+
+    }
+
+    private void test1() {
+        CountDownLatch latch = new CountDownLatch(10);
+
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        latch.countDown();
+    }
+
+}
