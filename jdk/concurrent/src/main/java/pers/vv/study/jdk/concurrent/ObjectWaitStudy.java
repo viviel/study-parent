a2093562800b593ebf77d25cb3ebd495adebe119
guoshixiong <viviel@163.com>
2020-04-25 22:52:26 +0800

    object wait method study

--- jdk/concurrent/src/main/java/pers/vv/study/jdk/concurrent/ObjectWaitStudy.java
+++ jdk/concurrent/src/main/java/pers/vv/study/jdk/concurrent/ObjectWaitStudy.java
@@ -4,14 +4,19 @@ public class ObjectWaitStudy {
 
     private final Object object = new Object();
 
+    private final Object o2 = new Object();
+
     public static void main(String[] args) {
         new ObjectWaitStudy().m1();
     }
 
+    /**
+     * 调用wait的对象必须是锁对象
+     */
     public void m1() {
         try {
             synchronized (object) {
-                object.wait();
+                o2.wait();
             }
         } catch (InterruptedException e) {
             e.printStackTrace();
