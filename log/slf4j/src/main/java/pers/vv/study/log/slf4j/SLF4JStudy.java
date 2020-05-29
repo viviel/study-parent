518fb762488ed099511b0a512d43afdbcb472f96
viviel <viviel@163.com>
2020-05-29 20:29:40 +0800

    test resource load

--- log/slf4j/src/main/java/pers/vv/study/log/slf4j/SLF4JStudy.java
+++ log/slf4j/src/main/java/pers/vv/study/log/slf4j/SLF4JStudy.java
@@ -3,11 +3,20 @@ package pers.vv.study.log.slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.Enumeration;
+
 public class SLF4JStudy {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Logger logger = LoggerFactory.getLogger(SLF4JStudy.class);
         logger.info("Hello World");
+
+        Enumeration<URL> r = SLF4JStudy.class.getClassLoader().getResources("org/slf4j/impl/StaticLoggerBinder.class");
+        while (r.hasMoreElements()) {
+            System.out.println(r.nextElement());
+        }
     }
 
 }
