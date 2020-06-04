c2c29b8debdac1c2ccd2655cbef743468e7a627f
viviel <viviel@163.com>
2020-06-04 22:21:41 +0800

    pb study

--- serialize/protocol-buffer/src/main/java/pers/vv/study/serialize/pb/Main.java
+++ serialize/protocol-buffer/src/main/java/pers/vv/study/serialize/pb/Main.java
@@ -1,4 +1,12 @@
 package pers.vv.study.serialize.pb;
 
+import com.google.protobuf.Descriptors;
+import org.apache.tubemq.corebase.protobuf.generated.ClientBroker;
+
 public class Main {
+
+    public static void main(String[] args) {
+        Descriptors.FileDescriptor descriptor = ClientBroker.getDescriptor();
+    }
+
 }
