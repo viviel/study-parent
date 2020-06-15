f05a4e04fabb648b46cca7e6571668e1dc47430a
viviel <viviel@163.com>
2020-06-15 23:24:39 +0800

    add module

---
+++ netty/src/main/java/pers/vv/study/netty/Server.java
@@ -0,0 +1,35 @@
+package pers.vv.study.netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class Server {
+
+    public static void main(String[] args) {
+        NioEventLoopGroup boss = new NioEventLoopGroup(1);
+        NioEventLoopGroup worker = new NioEventLoopGroup(3);
+        try {
+            final ServerBootstrap server = new ServerBootstrap();
+            server.group(boss, worker)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInboundHandlerAdapter() {
+                        @Override
+                        public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                            super.channelRead(ctx, msg);
+                        }
+                    });
+            ChannelFuture future = server.bind(8881).sync();
+            future.channel().closeFuture().sync();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            boss.shutdownGracefully();
+            worker.shutdownGracefully();
+        }
+    }
+
+}
