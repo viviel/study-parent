eedbe7e54a2b4cae2c46dae9c41918b3f326d12d
viviel <viviel@163.com>
2020-06-24 10:31:24 +0800

    format

--- schedule/quartz/src/main/java/pers/vv/study/schedule/quartz/HelloJob.java
+++ schedule/quartz/src/main/java/pers/vv/study/schedule/quartz/HelloJob.java
@@ -2,11 +2,10 @@ package pers.vv.study.schedule.quartz;
 
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
 
 public class HelloJob implements Job {
 
-    public void execute(JobExecutionContext context) throws JobExecutionException {
+    public void execute(JobExecutionContext context) {
         System.out.println("Hello Quartz~~~");
     }
 
--- schedule/quartz/src/main/java/pers/vv/study/schedule/quartz/QuartzMain.java
+++ schedule/quartz/src/main/java/pers/vv/study/schedule/quartz/QuartzMain.java
@@ -13,8 +13,14 @@ public class QuartzMain {
 
     public static void main(String[] args) throws SchedulerException {
         Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
-        JobDetail job = JobBuilder.newJob(HelloJob.class).withIdentity("job41", "group1").build();
-        Trigger trigger = TriggerBuilder.newTrigger().withIdentity("trigger1", "group1").startNow().withSchedule(SimpleScheduleBuilder.repeatSecondlyForever(1)).build();
+        JobDetail job = JobBuilder.newJob(HelloJob.class)
+                .withIdentity("job41", "group1")
+                .build();
+        Trigger trigger = TriggerBuilder.newTrigger()
+                .withIdentity("trigger1", "group1")
+                .startNow()
+                .withSchedule(SimpleScheduleBuilder.repeatSecondlyForever(1))
+                .build();
         scheduler.scheduleJob(job, trigger);
         scheduler.start();
     }
