f5b8ff8d4c1f21c9826a9385cbc8d871d9c38661
viviel <viviel@163.com>
2020-07-13 19:53:15 +0800

    zookeeper test

--- zookeeper/src/main/java/pers/viviel/study/zookeeper/Main.java
+++ zookeeper/src/main/java/pers/viviel/study/zookeeper/Main.java
@@ -1,4 +1,24 @@
 package pers.viviel.study.zookeeper;
 
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.recipes.cache.TreeCache;
+import org.apache.curator.retry.RetryNTimes;
+
 public class Main {
+
+    public static void main(String[] args) throws Exception {
+        String ADDRESS = "localhost:2181";
+        CuratorFramework client = CuratorFrameworkFactory.newClient(ADDRESS, new RetryNTimes(10, 5000));
+        client.start();
+
+        TreeCache tree = new TreeCache(client, "/vv");
+        tree.getListenable().addListener((c, event) -> {
+            System.out.println(event.getType().name());
+        });
+        tree.start();
+
+        Thread.sleep(Integer.MAX_VALUE);
+    }
+
 }
