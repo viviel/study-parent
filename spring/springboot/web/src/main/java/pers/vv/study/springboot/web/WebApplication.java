95bbb705b87c8e4163d5c908d8db37da7d3d4c59
viviel <viviel@163.com>
2020-08-06 20:51:23 +0800

    converter

--- spring/springboot/web/src/main/java/pers/vv/study/springboot/web/WebApplication.java
+++ spring/springboot/web/src/main/java/pers/vv/study/springboot/web/WebApplication.java
@@ -1,13 +1,21 @@
 package pers.vv.study.springboot.web;
 
+import com.google.common.cache.RemovalListener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.convert.converter.Converter;
 
 @SpringBootApplication
 public class WebApplication {
     
     public static void main(String[] args) {
         SpringApplication.run(WebApplication.class, args);
+    }
+    
+    @Bean
+    public Converter<String, Integer> converter() {
+        return source -> Integer.valueOf(source);
     }
     
 }
