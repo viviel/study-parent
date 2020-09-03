bba32501c3fb3d7c2825068017910c965f5485ab
viviel <viviel@163.com>
2020-09-03 16:43:09 +0800

    fix

--- socketio/src/main/java/pers/vv/study/socketio/SocketIOClient.java
+++ socketio/src/main/java/pers/vv/study/socketio/SocketIOClient.java
@@ -4,15 +4,16 @@ import io.socket.client.IO;
 import io.socket.client.Socket;
 
 import java.net.URISyntaxException;
-import java.util.concurrent.TimeUnit;
 
 public class SocketIOClient {
 
-    public static void main(String[] args) throws URISyntaxException, InterruptedException {
+    public static void main(String[] args) throws URISyntaxException {
         Socket socket = IO.socket("http://localhost:8888/vv");
+        socket.on(Socket.EVENT_CONNECT, args1 -> {
+            System.out.println("connected");
+            socket.disconnect();
+        });
         socket.connect();
-        TimeUnit.SECONDS.sleep(5);
-        socket.disconnect();
     }
 
 }
