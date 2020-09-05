4ca9359f173f8db33f927a0720ec00833098d2f6
viviel <viviel@163.com>
2020-09-05 16:28:52 +0800

    AQS study

--- jdk/lock/src/main/java/pers/vv/study/jdk/lock/MyReentrantLock.java
+++ jdk/lock/src/main/java/pers/vv/study/jdk/lock/MyReentrantLock.java
@@ -1,5 +1,6 @@
 package pers.vv.study.jdk.lock;
 
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -10,6 +11,21 @@ public class MyReentrantLock {
 
     public static void main(String[] args) {
         Lock lock = new ReentrantLock();
+        new Thread(runnable(lock), "thread1").start();
+        new Thread(runnable(lock), "thread2").start();
+    }
+
+    private static Runnable runnable(Lock lock) {
+        return () -> {
+            lock.lock();
+            try {
+                TimeUnit.SECONDS.sleep(Integer.MAX_VALUE);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
+            }
+        };
     }
 
 }
