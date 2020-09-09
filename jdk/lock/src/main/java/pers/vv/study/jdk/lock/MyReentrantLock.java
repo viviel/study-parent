6041ea73244f625ce99f089d989434e5b2ab51ef
viviel <viviel@163.com>
2020-09-09 11:06:35 +0800

    lock condition test

--- jdk/lock/src/main/java/pers/vv/study/jdk/lock/MyReentrantLock.java
+++ jdk/lock/src/main/java/pers/vv/study/jdk/lock/MyReentrantLock.java
@@ -1,6 +1,7 @@
 package pers.vv.study.jdk.lock;
 
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -10,12 +11,16 @@ import java.util.concurrent.locks.ReentrantLock;
 public class MyReentrantLock {
 
     public static void main(String[] args) {
+        new MyReentrantLock().condition();
+    }
+
+    private void lock() {
         Lock lock = new ReentrantLock();
         new Thread(runnable(lock), "thread1").start();
         new Thread(runnable(lock), "thread2").start();
     }
 
-    private static Runnable runnable(Lock lock) {
+    private Runnable runnable(Lock lock) {
         return () -> {
             lock.lock();
             try {
@@ -26,6 +31,26 @@ public class MyReentrantLock {
                 lock.unlock();
             }
         };
+    }
+
+    private void condition() {
+        Lock lock = new ReentrantLock();
+        Condition condition = lock.newCondition();
+        new Thread(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(5);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }, "thread1").start();
+        new Thread(() -> {
+            try {
+                condition.await();
+                System.out.println("thread2 down");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }, "thread2").start();
     }
 
 }
