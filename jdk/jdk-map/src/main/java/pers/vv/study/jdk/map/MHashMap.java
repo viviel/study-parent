e84d10e5f1dbc0ed709b54e6ce1e0563441172c3
viviel <viviel@163.com>
2020-09-23 16:37:10 +0800

    format

--- jdk/jdk-map/src/main/java/pers/vv/study/jdk/map/MHashMap.java
+++ jdk/jdk-map/src/main/java/pers/vv/study/jdk/map/MHashMap.java
@@ -1,12 +1,11 @@
 package pers.vv.study.jdk.map;
 
 import java.util.HashMap;
-import java.util.Map;
 
 public class MHashMap {
 
     public static void main(String[] args) {
-        Map<Object, Object> map = new HashMap<>();
+        HashMap<Object, Object> map = new HashMap<>();
         map.put(1, "vv");
         map.get(1);
     }
