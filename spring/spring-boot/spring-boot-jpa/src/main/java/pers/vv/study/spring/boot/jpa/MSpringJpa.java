76ab70faa461c9f2c4d3f965970f5f7ad745bc51
viviel <viviel@163.com>
2020-09-23 18:12:39 +0800

    add module spring jpa

---
+++ spring/spring-boot/spring-boot-jpa/src/main/java/pers/vv/study/spring/boot/jpa/MSpringJpa.java
@@ -0,0 +1,11 @@
+package pers.vv.study.spring.boot.jpa;
+
+import org.springframework.boot.SpringApplication;
+
+public class MSpringJpa {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MSpringJpa.class, args);
+    }
+
+}
