79032ce0b810357b8fdea2a61894d36e0415e352
viviel <viviel@163.com>
2020-10-23 11:25:59 +0800

    stream study

---
+++ jdk/jdk-syntax/src/main/java/pers/vv/study/jdk/syntax/MStream.java
@@ -0,0 +1,21 @@
+package pers.vv.study.jdk.syntax;
+
+import java.util.Collection;
+import java.util.Collections;
+
+/**
+ * java stream流原理：
+ * <li><a href="https://blog.csdn.net/lcgoing/article/details/87918010">深入理解Java8中Stream的实现原理</a></li>
+ */
+public class MStream {
+
+    public static void main(String[] args) {
+
+    }
+
+    private void test1() {
+        Collection<Object> c = Collections.singletonList(this);
+        c.stream().map(Object::toString).forEach(System.out::println);
+    }
+
+}
