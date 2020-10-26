a05add27e3b7cba3785d4522338fed8b80c9dd36
viviel <viviel@163.com>
2020-10-26 09:46:57 +0800

    MSpliterator

--- jdk/jdk-syntax/src/main/java/pers/vv/study/jdk/syntax/MSpliterator.java
+++ jdk/jdk-syntax/src/main/java/pers/vv/study/jdk/syntax/MSpliterator.java
@@ -1,19 +1,37 @@
 package pers.vv.study.jdk.syntax;
 
+import java.util.List;
 import java.util.Spliterator;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class MSpliterator {
 
+    private final IntStream stream;
+
+    private final List<Integer> list;
+
+    public MSpliterator() {
+        stream = IntStream.range(0, 100);
+        list = IntStream.range(0, 100).boxed().collect(Collectors.toList());
+    }
+
     public static void main(String[] args) {
         MSpliterator m = new MSpliterator();
         m.test1();
+        m.test2();
     }
 
     private void test1() {
-        IntStream stream = IntStream.range(0, 100);
         Spliterator.OfInt spliterator1 = stream.spliterator();
         Spliterator.OfInt spliterator2 = spliterator1.trySplit();
+        System.out.println(spliterator1.estimateSize());
+        System.out.println(spliterator2.estimateSize());
+    }
+
+    private void test2() {
+        Spliterator<Integer> spliterator1 = list.spliterator();
+        Spliterator<Integer> spliterator2 = spliterator1.trySplit();
         System.out.println(spliterator1.estimateSize());
         System.out.println(spliterator2.estimateSize());
     }
