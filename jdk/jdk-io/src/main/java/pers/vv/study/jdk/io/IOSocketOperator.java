08c4ac85877c25ace0c7d25bddc0351e9dd47e14
viviel <viviel@163.com>
2020-11-19 15:00:30 +0800

    IOSocketOperator

--- jdk/jdk-io/src/main/java/pers/vv/study/jdk/io/IOSocketOperator.java
+++ jdk/jdk-io/src/main/java/pers/vv/study/jdk/io/IOSocketOperator.java
@@ -1,4 +1,32 @@
 package pers.vv.study.jdk.io;
 
+import pers.vv.study.common.Utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
 public class IOSocketOperator {
+
+    public void start() throws IOException {
+        ServerSocket server = new ServerSocket();
+        while (!Thread.currentThread().isInterrupted()) {
+            Socket socket = server.accept();
+            Utils.cachedThreadPool().submit(() -> {
+                try {
+                    InputStream in = socket.getInputStream();
+                    int len;
+                    byte[] buf = new byte[1024];
+                    while ((len = in.read(buf)) != -1) {
+                        System.out.println(socket.getInetAddress().getHostAddress() + ": " +
+                                           new String(buf, 0, len, StandardCharsets.UTF_8));
+                    }
+                } catch (Exception ignore) {
+                }
+            });
+        }
+    }
+
 }
