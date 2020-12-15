6af75b7bdd1a8c33a47f4e3aa3d1e0ea0ab93809
viviel <viviel@163.com>
2020-12-15 11:05:22 +0800

    EventNotice

--- redis/redis-jedis/src/main/java/pers/vv/study/redis/jedis/EventNotice.java
+++ redis/redis-jedis/src/main/java/pers/vv/study/redis/jedis/EventNotice.java
@@ -1,15 +1,54 @@
 package pers.vv.study.redis.jedis;
 
+import pers.vv.study.common.Utils;
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPubSub;
 
 public class EventNotice {
 
-    private final Jedis jedis = new Jedis();
+    private final static String KEY = "vv:event:test";
+
+    private final Jedis jedis = new Jedis("localhost");
 
     public static void main(String[] args) {
+        EventNotice o = new EventNotice();
+        o.run();
+    }
 
+    public EventNotice() {
+        jedis.connect();
     }
 
     private void run() {
+        Utils.cachedThreadPool().submit(this::subscribe1);
+        Utils.cachedThreadPool().submit(this::subscribe2);
+    }
+
+    private void subscribe1() {
+        jedis.psubscribe(new JedisPubSub() {
+            @Override
+            public void onMessage(String channel, String message) {
+                System.out.println("channel = " + channel + ", message = " + message);
+            }
+
+            @Override
+            public void onPMessage(String pattern, String channel, String message) {
+                System.out.println("pattern = " + pattern + ", channel = " + channel + ", message = " + message);
+            }
+        }, "*");
+    }
+
+    private void subscribe2() {
+        jedis.psubscribe(new JedisPubSub() {
+            @Override
+            public void onMessage(String channel, String message) {
+                System.out.println("channel = " + channel + ", message = " + message);
+            }
+
+            @Override
+            public void onPMessage(String pattern, String channel, String message) {
+                System.out.println("pattern = " + pattern + ", channel = " + channel + ", message = " + message);
+            }
+        }, KEY);
     }
 }
