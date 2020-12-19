0146d66b41b5806d3b54589e572020ee5b5d66c6
viviel <viviel@163.com>
2020-12-19 14:50:14 +0800

    socket test

--- jdk/jdk-io/src/main/java/pers/vv/study/jdk/io/IOSocketOperator.java
+++ jdk/jdk-io/src/main/java/pers/vv/study/jdk/io/IOSocketOperator.java
@@ -4,14 +4,21 @@ import pers.vv.study.common.Utils;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 
 public class IOSocketOperator {
 
+    public static void main(String[] args) throws IOException {
+        IOSocketOperator o = new IOSocketOperator();
+        o.start();
+    }
+
     public void start() throws IOException {
         ServerSocket server = new ServerSocket();
+        server.bind(new InetSocketAddress(8080));
         while (!Thread.currentThread().isInterrupted()) {
             Socket socket = server.accept();
             Utils.cachedThreadPool().submit(() -> {
@@ -28,5 +35,4 @@ public class IOSocketOperator {
             });
         }
     }
-
 }
