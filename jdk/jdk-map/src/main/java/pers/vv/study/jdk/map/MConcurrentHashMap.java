1086595775c61ad0656bed9bb45e34444d3f881b
viviel <viviel@163.com>
2021-01-09 18:03:00 +0800

    MConcurrentHashMap: init

--- jdk/jdk-map/src/main/java/pers/vv/study/jdk/map/MConcurrentHashMap.java
+++ jdk/jdk-map/src/main/java/pers/vv/study/jdk/map/MConcurrentHashMap.java
@@ -1,14 +1,89 @@
 package pers.vv.study.jdk.map;
 
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ConcurrentMap;
 
-public class MConcurrentHashMap {
+public class MConcurrentHashMap<K, V> implements ConcurrentMap<K, V> {
 
-    public static void main(String[] args) {
-        ConcurrentMap<Object, Object> map = new ConcurrentHashMap<>();
-        map.put(1, "vv");
-        map.get(1);
+    @Override
+    public int size() {
+        return 0;
     }
 
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        return false;
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+        return false;
+    }
+
+    @Override
+    public V get(Object key) {
+        return null;
+    }
+
+    @Override
+    public V put(K key, V value) {
+        return null;
+    }
+
+    @Override
+    public V remove(Object key) {
+        return null;
+    }
+
+    @Override
+    public void putAll(Map<? extends K, ? extends V> m) {
+
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public Set<K> keySet() {
+        return null;
+    }
+
+    @Override
+    public Collection<V> values() {
+        return null;
+    }
+
+    @Override
+    public Set<Entry<K, V>> entrySet() {
+        return null;
+    }
+
+    @Override
+    public V putIfAbsent(K key, V value) {
+        return null;
+    }
+
+    @Override
+    public boolean remove(Object key, Object value) {
+        return false;
+    }
+
+    @Override
+    public boolean replace(K key, V oldValue, V newValue) {
+        return false;
+    }
+
+    @Override
+    public V replace(K key, V value) {
+        return null;
+    }
 }
