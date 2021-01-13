59556f94a06f1413931da36f1145ca51fa39a769
viviel <viviel@163.com>
2021-01-13 11:58:31 +0800

    MissingNumber

---
+++ algorithm/src/main/java/pers/algorithm/leetcode/bit/MissingNumber.java
@@ -0,0 +1,80 @@
+package pers.algorithm.leetcode.bit;
+
+import java.util.Arrays;
+
+/**
+ * 给定一个包含 [0, n]中n个数的数组 nums ，找出 [0, n] 这个范围内没有出现在数组中的那个数。
+ * <p>
+ * <p>
+ * <p>
+ * 进阶：
+ * <p>
+ * 你能否实现线性时间复杂度、仅使用额外常数空间的算法解决此问题?
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [3,0,1]
+ * 输出：2
+ * 解释：n = 3，因为有 3 个数字，所以所有的数字都在范围 [0,3] 内。2 是丢失的数字，因为它没有出现在 nums 中。
+ * 示例 2：
+ * <p>
+ * 输入：nums = [0,1]
+ * 输出：2
+ * 解释：n = 2，因为有 2 个数字，所以所有的数字都在范围 [0,2] 内。2 是丢失的数字，因为它没有出现在 nums 中。
+ * 示例 3：
+ * <p>
+ * 输入：nums = [9,6,4,2,3,5,7,0,1]
+ * 输出：8
+ * 解释：n = 9，因为有 9 个数字，所以所有的数字都在范围 [0,9] 内。8 是丢失的数字，因为它没有出现在 nums 中。
+ * 示例 4：
+ * <p>
+ * 输入：nums = [0]
+ * 输出：1
+ * 解释：n = 1，因为有 1 个数字，所以所有的数字都在范围 [0,1] 内。1 是丢失的数字，因为它没有出现在 nums 中。
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * n == nums.length
+ * 1 <= n <= 10^4
+ * 0 <= nums[i] <= n
+ * nums 中的所有数字都 独一无二
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/missing-number
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+class MissingNumber {
+
+    public int missingNumber(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != i) {
+                return i;
+            }
+        }
+        return nums.length;
+    }
+
+    public int missingNumber2(int[] nums) {
+        int total = (1 + nums.length) * nums.length / 2;
+        int sum = 0;
+        for (int e : nums) {
+            sum += e;
+        }
+        return total - sum;
+    }
+
+    /**
+     * 1^1^2^2^3 = 3
+     */
+    int missingNumber3(int[] nums) {
+        int res = nums.length;
+        for (int i = 0; i < nums.length; ++i) {
+            res ^= nums[i];
+            res ^= i;
+        }
+        return res;
+    }
+}
