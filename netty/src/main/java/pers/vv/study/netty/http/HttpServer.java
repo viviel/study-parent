6e4f0d812c8a89c42a1c7888672983798b6d4911
viviel <viviel@163.com>
2021-01-13 17:27:09 +0800

    Netty http server

--- netty/src/main/java/pers/vv/study/netty/http/HttpServer.java
+++ netty/src/main/java/pers/vv/study/netty/http/HttpServer.java
@@ -1,15 +1,13 @@
 package pers.vv.study.netty.http;
 
 import io.netty.bootstrap.ServerBootstrap;
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 
 public class HttpServer {
 
     public static void main(String[] args) {
-
+        new HttpServer().start();
     }
 
     public void start() {
@@ -19,18 +17,10 @@ public class HttpServer {
             ServerBootstrap server = new ServerBootstrap();
             server.group(boss, worker)
                     .channel(NioServerSocketChannel.class)
-                    .childHandler(new ChannelInboundHandlerAdapter() {
-                        @Override
-                        public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-                            super.channelRead(ctx, msg);
-                        }
-                    });
+                    .childHandler(new HttpServerInitializer());
             server.bind(80).sync();
         } catch (Exception e) {
             e.printStackTrace();
-        } finally {
-            boss.shutdownGracefully();
-            worker.shutdownGracefully();
         }
     }
 }
