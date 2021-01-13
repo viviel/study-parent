a76257d7e71445e438892ba039e414bcc31bda44
viviel <viviel@163.com>
2021-01-13 17:27:09 +0800

    Netty http server

---
+++ netty/src/main/java/pers/vv/study/netty/http/HttpServerInitializer.java
@@ -0,0 +1,18 @@
+package pers.vv.study.netty.http;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.HttpServerExpectContinueHandler;
+
+public class HttpServerInitializer extends ChannelInitializer<SocketChannel> {
+
+    @Override
+    protected void initChannel(SocketChannel channel) throws Exception {
+        ChannelPipeline pipeline = channel.pipeline();
+        pipeline.addLast("httpServerCodec", new HttpServerCodec());
+//        pipeline.addLast("httpServerExpectContinueHandler", new HttpServerExpectContinueHandler());
+        pipeline.addLast("httpHandler", new HttpHandler());
+    }
+}
