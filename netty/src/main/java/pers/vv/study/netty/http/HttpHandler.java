714418288949ad50ab449c3b98a259c1fe1c69a5
viviel <viviel@163.com>
2021-01-22 15:49:13 +0800

    Netty add log handler

--- netty/src/main/java/pers/vv/study/netty/http/HttpHandler.java
+++ netty/src/main/java/pers/vv/study/netty/http/HttpHandler.java
@@ -25,14 +25,12 @@ public class HttpHandler extends SimpleChannelInboundHandler<HttpObject> {
     public void channelRead0(ChannelHandlerContext ctx, HttpObject msg) {
         if (msg instanceof HttpRequest) {
             HttpRequest req = (HttpRequest) msg;
-
             boolean keepAlive = HttpUtil.isKeepAlive(req);
             FullHttpResponse response = new DefaultFullHttpResponse(req.protocolVersion(), OK,
                                                                     Unpooled.wrappedBuffer(CONTENT));
             response.headers()
                     .set(CONTENT_TYPE, TEXT_PLAIN)
                     .setInt(CONTENT_LENGTH, response.content().readableBytes());
-
             if (keepAlive) {
                 if (!req.protocolVersion().isKeepAliveDefault()) {
                     response.headers().set(CONNECTION, KEEP_ALIVE);
@@ -41,9 +39,7 @@ public class HttpHandler extends SimpleChannelInboundHandler<HttpObject> {
                 // Tell the client we're going to close the connection.
                 response.headers().set(CONNECTION, CLOSE);
             }
-
             ChannelFuture f = ctx.write(response);
-
             if (!keepAlive) {
                 f.addListener(ChannelFutureListener.CLOSE);
             }
