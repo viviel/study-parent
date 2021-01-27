adcf345f71cca99fdf377b5e548e377680da07b0
viviel <viviel@163.com>
2021-01-27 10:43:09 +0800

    FindComplement

---
+++ algorithm/src/test/java/pers/algorithm/leetcode/bit/FindComplementTest.java
@@ -0,0 +1,17 @@
+package pers.algorithm.leetcode.bit;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FindComplementTest {
+
+    private final FindComplement o = new FindComplement();
+
+    @Test
+    void test1() {
+        assertEquals(2, o.findComplement(5));
+        assertEquals(0, o.findComplement(1));
+        assertEquals(0, o.findComplement(0));
+    }
+}
