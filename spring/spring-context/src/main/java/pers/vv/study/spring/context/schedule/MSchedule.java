568f8f79113554e0ac081a168c0bd98e100476bd
viviel <viviel@163.com>
2021-01-29 20:54:18 +0800

    Spring schedule test

---
+++ spring/spring-context/src/main/java/pers/vv/study/spring/context/schedule/MSchedule.java
@@ -0,0 +1,40 @@
+package pers.vv.study.spring.context.schedule;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MSchedule {
+
+    @Bean
+    public TaskScheduler schedule() {
+        ThreadPoolTaskScheduler executor = new ThreadPoolTaskScheduler();
+        executor.setPoolSize(10);
+        return executor;
+    }
+
+    @Scheduled(fixedDelay = 1)
+    public void test1() {
+        System.out.println("MSchedule.test1");
+        System.out.println(Thread.currentThread().getName());
+        try {
+            Thread.sleep(1000 * 5);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Scheduled(fixedDelay = 1)
+    public void test2() {
+        System.out.println("MSchedule.test2");
+        System.out.println(Thread.currentThread().getName());
+        try {
+            Thread.sleep(1000 * 5);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
