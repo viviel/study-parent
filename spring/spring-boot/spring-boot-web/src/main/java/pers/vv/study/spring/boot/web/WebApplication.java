c569c69608c5e262312e1d1768f46f4fcaebd9ea
viviel <viviel@163.com>
2021-01-29 19:50:28 +0800

    Format

--- spring/spring-boot/spring-boot-web/src/main/java/pers/vv/study/spring/boot/web/WebApplication.java
+++ spring/spring-boot/spring-boot-web/src/main/java/pers/vv/study/spring/boot/web/WebApplication.java
@@ -5,14 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WebApplication {
-    
+
     public static void main(String[] args) {
         SpringApplication.run(WebApplication.class, args);
     }
-    
+
 //    @Bean
 //    public Converter<String, Integer> myConverter() {
 //        return Integer::valueOf;
 //    }
-    
 }
