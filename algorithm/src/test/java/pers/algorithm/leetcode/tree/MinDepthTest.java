7b94a05ffb9c6443b6d373f631ba8a2326e440b9
viviel <viviel@163.com>
2021-02-05 17:27:12 +0800

    MinDepth

---
+++ algorithm/src/test/java/pers/algorithm/leetcode/tree/MinDepthTest.java
@@ -0,0 +1,17 @@
+package pers.algorithm.leetcode.tree;
+
+import org.junit.jupiter.api.Test;
+import pers.algorithm.leetcode.tree.common.TreeUtils;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MinDepthTest {
+
+    private final MinDepth o = new MinDepth();
+
+    @Test
+    void test() {
+        assertEquals(2, o.minDepth(TreeUtils.generateTree8()));
+        assertEquals(5, o.minDepth(TreeUtils.generateTree10()));
+    }
+}
