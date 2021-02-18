eed1157bbb453688c35bebd27174832b499e9d30
viviel <viviel@163.com>
2021-02-18 15:17:08 +0800

    String boot config

---
+++ spring/spring-boot/spring-boot-web/src/main/java/pers/vv/study/spring/boot/web/WebConfig.java
@@ -0,0 +1,12 @@
+package pers.vv.study.spring.boot.web;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebConfig {
+
+//    @Bean
+//    public Converter<String, Integer> myConverter() {
+//        return Integer::valueOf;
+//    }
+}
