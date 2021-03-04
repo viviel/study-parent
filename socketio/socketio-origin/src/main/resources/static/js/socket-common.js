d8730e335d457a467764365f64a2417b8e6ca323
viviel <viviel@163.com>
2021-03-04 19:49:58 +0800

    SocketIO html

---
+++ socketio/socketio-origin/src/main/resources/static/js/socket-common.js
@@ -0,0 +1,57 @@
+function setSign(c) {
+    $('#sign').html(JSON.stringify(c, null, 2));
+}
+
+function setContent(c) {
+    $('#content').html(c);
+}
+
+function appendContent(c) {
+    let $content = $('#content');
+    const content = $content.html();
+    $content.html(content + JSON.stringify(c, null, 2) + '<br><br>');
+}
+
+function addEvent() {
+    socket.on('connect', function () {
+        setSign('connect');
+    });
+
+    socket.on('disconnect', function () {
+        setSign('disconnect');
+    });
+
+    socket.on('connect_error', function (data) {
+        setSign(data);
+    });
+
+    socket.on('connectData', function (data) {
+        appendContent(data);
+    });
+
+    socket.on('message', function (data) {
+        appendContent(data);
+    });
+}
+
+function test() {
+    appendContent('test');
+}
+
+function sendMsg() {
+    let time = parseInt(new Date().getTime() / 1000);
+    let msg = {
+        "msg_type": 1,
+        "id": 'messageId-' + time,
+        "live_id": liveId,
+        "role": 1,
+        "action": 0,
+        "sender_id": openId,
+        "sender_name": "vv",
+        "msg": "弹幕test",
+        "time": time
+    };
+    socket.emit('message', msg, function (data) {
+        appendContent(data);
+    });
+}
