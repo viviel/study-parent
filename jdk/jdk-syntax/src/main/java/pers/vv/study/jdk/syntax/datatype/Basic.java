9946b35a0978b75b80a453b8f47877d18979e4e6
viviel <viviel@163.com>
2021-03-09 15:44:57 +0800

    Basic data type

---
+++ jdk/jdk-syntax/src/main/java/pers/vv/study/jdk/syntax/datatype/Basic.java
@@ -0,0 +1,31 @@
+package pers.vv.study.jdk.syntax.datatype;
+
+import lombok.Data;
+
+@Data
+public class Basic {
+
+    // 16
+    private short s = 0;
+
+    // 32
+    private int i = 0;
+
+    // 64
+    private long l = 0L;
+
+    // 32
+    private float f = 0.0F;
+
+    // 64
+    private double d = 0.0;
+
+    // 8
+    private byte b = 'a';
+
+    // 16
+    private char c = '你';
+
+    // ? 不确定
+    private boolean bl = true;
+}
