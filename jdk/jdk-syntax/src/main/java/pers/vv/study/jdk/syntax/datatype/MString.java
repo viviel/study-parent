b14dc664d21c8d464d5734cd2062e434328cf2d1
viviel <viviel@163.com>
2021-03-09 20:01:02 +0800

    MString

---
+++ jdk/jdk-syntax/src/main/java/pers/vv/study/jdk/syntax/datatype/MString.java
@@ -0,0 +1,57 @@
+package pers.vv.study.jdk.syntax.datatype;
+
+import java.util.Random;
+
+/**
+ * String.intern()
+ * https://tech.meituan.com/2014/03/06/in-depth-understanding-string-intern.html
+ */
+public class MString {
+
+    public static void main(String[] args) {
+        String s = new String("abc");
+    }
+
+    public void test1() {
+        String s = new String("1");
+        s.intern();
+        String s2 = "1";
+        System.out.println(s == s2);
+
+        String s3 = new String("1") + new String("1");
+        s3.intern();
+        String s4 = "11";
+        System.out.println(s3 == s4);
+    }
+
+    public void test2() {
+        String s = new String("1");
+        String s2 = "1";
+        s.intern();
+        System.out.println(s == s2);
+
+        String s3 = new String("1") + new String("1");
+        String s4 = "11";
+        s3.intern();
+        System.out.println(s3 == s4);
+    }
+
+    public void test3() {
+        int MAX = 1000 * 10000;
+        String[] arr = new String[MAX];
+
+        Integer[] DB_DATA = new Integer[10];
+        Random random = new Random(10 * 10000);
+        for (int i = 0; i < DB_DATA.length; i++) {
+            DB_DATA[i] = random.nextInt();
+        }
+        long t = System.currentTimeMillis();
+        for (int i = 0; i < MAX; i++) {
+            //arr[i] = new String(String.valueOf(DB_DATA[i % DB_DATA.length]));
+            arr[i] = new String(String.valueOf(DB_DATA[i % DB_DATA.length])).intern();
+        }
+
+        System.out.println((System.currentTimeMillis() - t) + "ms");
+        System.gc();
+    }
+}
