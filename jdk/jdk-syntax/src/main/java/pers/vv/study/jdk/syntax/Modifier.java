040a68de53d60ca5704c0c7131085f65d1db63e9
viviel <viviel@163.com>
2021-03-10 11:50:56 +0800

    Modifier

---
+++ jdk/jdk-syntax/src/main/java/pers/vv/study/jdk/syntax/Modifier.java
@@ -0,0 +1,25 @@
+package pers.vv.study.jdk.syntax;
+
+/**
+ * 修饰符
+ */
+public class Modifier {
+
+    public void test1() {
+    }
+
+    /**
+     * 包级/子类可见
+     */
+    protected void test3() {
+    }
+
+    /**
+     * 包级可见
+     */
+    void test2() {
+    }
+
+    private void test4() {
+    }
+}
