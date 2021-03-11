d5d7697b70b676bb34ed2cf1e8987bee317bc842
viviel <viviel@163.com>
2021-03-11 13:17:35 +0800

    Preorder

---
+++ algorithm/src/main/java/pers/algorithm/leetcode/tree/Preorder.java
@@ -0,0 +1,47 @@
+package pers.algorithm.leetcode.tree;
+
+import pers.algorithm.leetcode.tree.common.Node;
+
+import java.util.*;
+
+class Preorder {
+
+    public List<Integer> preorder(Node root) {
+        List<Integer> result = new ArrayList<>();
+        traversal(root, result);
+        return result;
+    }
+
+    private void traversal(Node root, List<Integer> result) {
+        if (null == root) {
+            return;
+        }
+        result.add(root.val);
+        if (null == root.children) {
+            return;
+        }
+        for (Node e : root.children) {
+            traversal(e, result);
+        }
+    }
+
+    public List<Integer> preorder2(Node root) {
+        List<Integer> result = new ArrayList<>();
+        if (null == root) {
+            return result;
+        }
+        Stack<Node> s = new Stack<>();
+        s.push(root);
+        while (!s.isEmpty()) {
+            Node node = s.pop();
+            result.add(node.val);
+            if (null == node.children) {
+                continue;
+            }
+            for (int i = node.children.size() - 1; i >= 0; i--) {
+                s.push(node.children.get(i));
+            }
+        }
+        return result;
+    }
+}
---
+++ algorithm/src/test/java/pers/algorithm/leetcode/tree/PreorderTest.java
@@ -0,0 +1,12 @@
+package pers.algorithm.leetcode.tree;
+
+import org.junit.jupiter.api.Test;
+
+class PreorderTest {
+
+    private final Preorder o = new Preorder();
+
+    @Test
+    void test1() {
+    }
+}
