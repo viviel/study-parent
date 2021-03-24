45d25793a35043c617d48b2ab90fe1f8f69c8618
viviel <viviel@163.com>
2021-03-24 10:50:45 +0800

    SearchInsert

---
+++ algorithm/alg-leetcode/src/main/java/pers/algorithm/leetcode/division/SearchInsert.java
@@ -0,0 +1,52 @@
+package pers.algorithm.leetcode.division;
+
+/**
+ * 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+ * <p>
+ * 你可以假设数组中无重复元素。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: [1,3,5,6], 5
+ * <p>
+ * 输出: 2
+ * 示例2:
+ * <p>
+ * 输入: [1,3,5,6], 2
+ * 输出: 1
+ * <p>
+ * 示例 3:
+ * <p>
+ * 输入: [1,3,5,6], 7
+ * 输出: 4
+ * <p>
+ * 示例 4:
+ * <p>
+ * 输入: [1,3,5,6], 0
+ * 输出: 0
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/search-insert-position
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+class SearchInsert {
+
+    public int searchInsert(int[] nums, int target) {
+        int i = 0, j = nums.length - 1;
+        while (i < j) {
+            int m = (i + j) / 2;
+            if (nums[m] == target) {
+                return m;
+            } else if (nums[m] < target) {
+                i = m + 1;
+            } else {
+                j = m - 1;
+            }
+        }
+        if (target > nums[i]) {
+            return i + 1;
+        } else {
+            return i;
+        }
+    }
+}
---
+++ algorithm/alg-leetcode/src/test/java/pers/algorithm/leetcode/division/SearchInsertTest.java
@@ -0,0 +1,19 @@
+package pers.algorithm.leetcode.division;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SearchInsertTest {
+
+    private final SearchInsert o = new SearchInsert();
+
+    @Test
+    void test1() {
+        assertEquals(2, o.searchInsert(new int[]{1, 3, 5, 6}, 5));
+        assertEquals(1, o.searchInsert(new int[]{1, 3, 5, 6}, 2));
+        assertEquals(4, o.searchInsert(new int[]{1, 3, 5, 6}, 7));
+        assertEquals(0, o.searchInsert(new int[]{1, 3, 5, 6}, 0));
+        assertEquals(1, o.searchInsert(new int[]{1, 3}, 2));
+    }
+}
