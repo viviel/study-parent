ca5354901b41e4436b4712ab7cc1c61e12acfff4
viviel <viviel@163.com>
2021-03-24 10:50:45 +0800

    SearchInsert

--- java/java-jdk/jdk-concurrent/src/test/java/pers/vv/study/jdk/concurrent/ExecutorTest.java
+++ java/java-jdk/jdk-concurrent/src/test/java/pers/vv/study/jdk/concurrent/ExecutorTest.java
@@ -1,4 +1,41 @@
 package pers.vv.study.jdk.concurrent;
 
+import org.junit.jupiter.api.Test;
+import pers.vv.study.common.Utils;
+
+import java.util.concurrent.*;
+
 public class ExecutorTest {
+
+    @Test
+    void test1() throws InterruptedException {
+        ExecutorService es = Executors.newCachedThreadPool();
+        es.submit(() -> {
+            try {
+                Utils.sleep(1000 * 10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("done");
+        });
+        es.shutdown();
+        es.awaitTermination(Integer.MAX_VALUE, TimeUnit.SECONDS);
+    }
+
+    @Test
+    void test2() throws InterruptedException {
+        ThreadPoolExecutor es = new ThreadPoolExecutor(
+                0, Integer.MAX_VALUE, 60L, TimeUnit.SECONDS, new SynchronousQueue<>()
+        );
+        es.execute(() -> {
+            try {
+                Utils.sleep(1000 * 10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("done");
+        });
+        es.shutdown();
+        es.awaitTermination(Integer.MAX_VALUE, TimeUnit.SECONDS);
+    }
 }
