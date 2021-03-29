926d9fbff27f4f1444eb13078247fb445d0eab35
viviel <viviel@163.com>
2021-03-29 10:25:51 +0800

    ArrangeCoins

---
+++ algorithm/alg-leetcode/src/main/java/pers/algorithm/leetcode/division/ArrangeCoins.java
@@ -0,0 +1,55 @@
+package pers.algorithm.leetcode.division;
+
+/**
+ * 你总共有n枚硬币，你需要将它们摆成一个阶梯形状，第k行就必须正好有k枚硬币。
+ * <p>
+ * 给定一个数字n，找出可形成完整阶梯行的总行数。
+ * <p>
+ * n是一个非负整数，并且在32位有符号整型的范围内。
+ * <p>
+ * 示例 1:
+ * <p>
+ * n = 5
+ * <p>
+ * 硬币可排列成以下几行:
+ * ¤
+ * ¤ ¤
+ * ¤ ¤
+ * <p>
+ * 因为第三行不完整，所以返回2.
+ * <p>
+ * 示例 2:
+ * <p>
+ * n = 8
+ * <p>
+ * 硬币可排列成以下几行:
+ * ¤
+ * ¤ ¤
+ * ¤ ¤ ¤
+ * ¤ ¤
+ * <p>
+ * 因为第四行不完整，所以返回3.
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/arranging-coins
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+class ArrangeCoins {
+
+    public int arrangeCoins(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        int s = n;
+        for (int i = 1; i <= n; i++) {
+            s -= i;
+            if (s < 0) {
+                return i - 1;
+            }
+        }
+        return 0;
+    }
+}
