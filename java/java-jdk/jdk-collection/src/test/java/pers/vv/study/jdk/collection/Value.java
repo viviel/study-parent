c750587da2e68e833bd9a275c241b7a870808f1e
viviel <viviel@163.com>
2021-04-12 19:43:23 +0800

    SetTest

---
+++ java/java-jdk/jdk-collection/src/test/java/pers/vv/study/jdk/collection/Value.java
@@ -0,0 +1,13 @@
+package pers.vv.study.jdk.collection;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Value {
+
+    private Integer id;
+
+    private String desc;
+}
