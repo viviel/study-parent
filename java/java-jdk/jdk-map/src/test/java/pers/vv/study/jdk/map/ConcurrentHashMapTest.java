5d37db995f33785eb8d996cb595aac4a741b2922
viviel <viviel@163.com>
2021-04-15 17:23:22 +0800

    Test ConcurrentHashMap computeIfAbsent

--- java/java-jdk/jdk-map/src/test/java/pers/vv/study/jdk/map/ConcurrentHashMapTest.java
+++ java/java-jdk/jdk-map/src/test/java/pers/vv/study/jdk/map/ConcurrentHashMapTest.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import org.junit.jupiter.api.Test;
 import pers.vv.study.common.Utils;
 
+import java.util.ArrayList;
 import java.util.concurrent.ConcurrentHashMap;
 
 class ConcurrentHashMapTest {
@@ -36,5 +37,38 @@ class ConcurrentHashMapTest {
             int size = map.size();
             System.out.println(size);
         };
+    }
+
+    /**
+     * https://bugs.openjdk.java.net/browse/JDK-8161372
+     * <p>
+     * 测试computeIfAbsent
+     * jdk1.8  测试两次，平均39
+     * jdk9    测试两次，平均17
+     * <p>
+     * 代码改动点较少，改动代码逻辑未知
+     */
+    @Test
+    void test2() throws InterruptedException {
+        int MAP_SIZE = 20;
+        int THREADS = 20;
+        ConcurrentHashMap<Integer, Integer> map = new ConcurrentHashMap<>();
+
+        for (int i = 0; i < MAP_SIZE; i++) map.put(i, i);
+
+        ArrayList<Thread> threads = new ArrayList<>();
+        for (int i = 0; i < THREADS; i++) {
+            Thread t = new Thread(() -> {
+                int n = 0;
+                int result = 0;
+                while (result < Integer.MAX_VALUE >> 1) {
+                    n = (n + 1) % MAP_SIZE;
+                    result += map.computeIfAbsent(n, (key) -> key + key);
+                }
+            });
+            threads.add(t);
+            t.start();
+        }
+        threads.get(0).join();
     }
 }
