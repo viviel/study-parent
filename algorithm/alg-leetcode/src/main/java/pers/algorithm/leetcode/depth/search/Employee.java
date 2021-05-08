c41ef371f81b14e9130c6fad6bdd7671ca03ebe0
viviel <viviel@163.com>
2021-05-08 11:16:23 +0800

    GetImportance

---
+++ algorithm/alg-leetcode/src/main/java/pers/algorithm/leetcode/depth/search/Employee.java
@@ -0,0 +1,10 @@
+package pers.algorithm.leetcode.depth.search;
+
+import java.util.List;
+
+class Employee {
+
+    public int id;
+    public int importance;
+    public List<Integer> subordinates;
+}
