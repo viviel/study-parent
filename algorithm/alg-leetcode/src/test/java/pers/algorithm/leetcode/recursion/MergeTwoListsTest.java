4f0118e9cb0d56ffb17376a7d71f67355762ae59
viviel <viviel@163.com>
2021-05-13 10:20:43 +0800

    MergeTwoLists

---
+++ algorithm/alg-leetcode/src/test/java/pers/algorithm/leetcode/recursion/MergeTwoListsTest.java
@@ -0,0 +1,15 @@
+package pers.algorithm.leetcode.recursion;
+
+import org.junit.jupiter.api.Test;
+import pers.algorithm.leetcode.recursion.MergeTwoLists;
+import pers.algorithm.leetcode.tree.search.ListNode;
+
+class MergeTwoListsTest {
+
+    private final MergeTwoLists o = new MergeTwoLists();
+
+    @Test
+    void test1() {
+        ListNode r = o.mergeTwoLists(null, null);
+    }
+}
