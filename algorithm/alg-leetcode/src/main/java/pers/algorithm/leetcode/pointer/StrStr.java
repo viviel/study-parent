8c2918c96f44714bb5ecc248771af5530c8d1004
viviel <viviel@163.com>
2021-05-27 11:05:38 +0800

    StrStr

---
+++ algorithm/alg-leetcode/src/main/java/pers/algorithm/leetcode/pointer/StrStr.java
@@ -0,0 +1,70 @@
+package pers.algorithm.leetcode.pointer;
+
+/**
+ * 实现strStr()函数。
+ * <p>
+ * 给你两个字符串haystack 和 needle ，请你在 haystack 字符串中找出 needle 字符串出现的第一个位置（下标从 0 开始）。
+ * 如果不存在，则返回 -1 。
+ * <p>
+ * <p>
+ * <p>
+ * 说明：
+ * <p>
+ * 当needle是空字符串时，我们应当返回什么值呢？这是一个在面试中很好的问题。
+ * <p>
+ * 对于本题而言，当needle是空字符串时我们应当返回 0 。这与 C 语言的strstr()以及 Java 的indexOf()定义相符。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：haystack = "hello", needle = "ll"
+ * 输出：2
+ * 示例 2：
+ * <p>
+ * 输入：haystack = "aaaaa", needle = "bba"
+ * 输出：-1
+ * 示例 3：
+ * <p>
+ * 输入：haystack = "", needle = ""
+ * 输出：0
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 0 <= haystack.length, needle.length <= 5 * 10^4
+ * haystack 和 needle 仅由小写英文字符组成
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/implement-strstr
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+class StrStr {
+
+    public int strStr(String haystack, String needle) {
+        if (needle.length() == 0) {
+            return 0;
+        }
+        for (int i = 0; i < haystack.length(); i++) {
+            for (int j = 0; j < needle.length(); j++) {
+                if ((i + j) >= haystack.length()) {
+                    return -1;
+                }
+                if (haystack.charAt(i + j) != needle.charAt(j)) {
+                    break;
+                }
+                if (j == needle.length() - 1) {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * KMP算法
+     */
+    public int strStr2(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+}
