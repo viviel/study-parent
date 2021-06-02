f1afbc80ccebec6c9183e00c0d8aa5d9817e4478
viviel <viviel@163.com>
2021-06-02 17:36:16 +0800

    Test spring destroy method

--- java/java-spring/spring-context/src/main/java/pers/vv/study/spring/context/SpringConfig.java
+++ java/java-spring/spring-context/src/main/java/pers/vv/study/spring/context/SpringConfig.java
@@ -1,11 +1,32 @@
 package pers.vv.study.spring.context;
 
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.List;
+
 @EnableScheduling
 @EnableAspectJAutoProxy
 @Configuration
-public class SpringConfig {
+public class SpringConfig implements InitializingBean {
+
+    @Autowired
+    private List<DisposableBean> disposableBeans;
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            for (DisposableBean disposableBean : disposableBeans) {
+                try {
+                    disposableBean.destroy();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }));
+    }
 }
