b50862b540356e01c5d057b220c5049c3ce80f3a
viviel <viviel@163.com>
2021-06-04 15:35:40 +0800

    Remove module socketio-x

---
+++ java/java-spring/spring-context/src/test/java/pers/vv/study/spring/context/aop/AopTest.java
@@ -0,0 +1,33 @@
+package pers.vv.study.spring.context.aop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+@SpringJUnitConfig(classes = AopTest.Config.class)
+class AopTest {
+
+    @EnableAspectJAutoProxy
+    static class Config {
+
+        @Bean
+        public LogAspects logAspects() {
+            return new LogAspects();
+        }
+
+        @Bean
+        public MathCalculator mathCalculator() {
+            return new MathCalculator();
+        }
+    }
+
+    @Autowired
+    private MathCalculator mathCalculator;
+
+    @Test
+    void test1() {
+        int r = mathCalculator.div(2, 1);
+    }
+}
