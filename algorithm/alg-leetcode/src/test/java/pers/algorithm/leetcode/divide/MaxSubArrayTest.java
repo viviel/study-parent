ef20bcc7c17f61b012dea2714341b5e7e1fa6df9
viviel <viviel@163.com>
2021-06-09 15:09:51 +0800

    MaxSubArray

---
+++ algorithm/alg-leetcode/src/test/java/pers/algorithm/leetcode/divide/MaxSubArrayTest.java
@@ -0,0 +1,21 @@
+package pers.algorithm.leetcode.divide;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MaxSubArrayTest {
+
+    private MaxSubArray o;
+
+    @BeforeEach
+    void setUp() {
+        o = new MaxSubArray();
+    }
+
+    @Test
+    void test1() {
+
+    }
+}
