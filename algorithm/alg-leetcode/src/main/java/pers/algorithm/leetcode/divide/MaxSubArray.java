8e556a693a6828ae697ec27bdbd87e82b39ee308
viviel <viviel@163.com>
2021-06-09 15:09:51 +0800

    MaxSubArray

---
+++ algorithm/alg-leetcode/src/main/java/pers/algorithm/leetcode/divide/MaxSubArray.java
@@ -0,0 +1,98 @@
+package pers.algorithm.leetcode.divide;
+
+/**
+ * https://leetcode-cn.com/problems/maximum-subarray/solution/zui-da-zi-xu-he-by-leetcode-solution/
+ * <p>
+ * 给定一个整数数组 nums，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [-2,1,-3,4,-1,2,1,-5,4]
+ * 输出：6
+ * 解释：连续子数组[4,-1,2,1] 的和最大，为6 。
+ * 示例 2：
+ * <p>
+ * 输入：nums = [1]
+ * 输出：1
+ * 示例 3：
+ * <p>
+ * 输入：nums = [0]
+ * 输出：0
+ * 示例 4：
+ * <p>
+ * 输入：nums = [-1]
+ * 输出：-1
+ * 示例 5：
+ * <p>
+ * 输入：nums = [-100000]
+ * 输出：-100000
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= nums.length <= 3 * 10^4
+ * -10^5 <= nums[i] <= 10^5
+ * <p>
+ * <p>
+ * 进阶：如果你已经实现复杂度为 O(n) 的解法，尝试使用更为精妙的 分治法 求解。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/maximum-subarray
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+class MaxSubArray {
+
+    /**
+     * 动态规划
+     * <p>
+     * f(n) = max(f(n - 1) + nums[n], nums[n])
+     * <p>
+     * -2,1,-3,4,-1,2,1,-5,4
+     * f(0) = -2
+     * f(1) = max(f(0) + 1, 1) = 1
+     * f(2) = max(f(1) + -3, -3) = -2
+     */
+    public int maxSubArray(int[] nums) {
+        int pre = 0, maxAns = nums[0];
+        for (int x : nums) {
+            pre = Math.max(pre + x, x);
+            maxAns = Math.max(maxAns, pre);
+        }
+        return maxAns;
+    }
+
+    private static class Status {
+        public int lSum, rSum, mSum, iSum;
+
+        public Status(int lSum, int rSum, int mSum, int iSum) {
+            this.lSum = lSum;
+            this.rSum = rSum;
+            this.mSum = mSum;
+            this.iSum = iSum;
+        }
+    }
+
+    public int maxSubArray2(int[] nums) {
+        return getInfo(nums, 0, nums.length - 1).mSum;
+    }
+
+    public Status getInfo(int[] a, int l, int r) {
+        if (l == r) {
+            return new Status(a[l], a[l], a[l], a[l]);
+        }
+        int m = (l + r) >> 1;
+        Status lSub = getInfo(a, l, m);
+        Status rSub = getInfo(a, m + 1, r);
+        return pushUp(lSub, rSub);
+    }
+
+    public Status pushUp(Status l, Status r) {
+        int iSum = l.iSum + r.iSum;
+        int lSum = Math.max(l.lSum, l.iSum + r.lSum);
+        int rSum = Math.max(r.rSum, r.iSum + l.rSum);
+        int mSum = Math.max(Math.max(l.mSum, r.mSum), l.rSum + r.lSum);
+        return new Status(lSum, rSum, mSum, iSum);
+    }
+}
