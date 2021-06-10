e6dfda5030dbe1e8825789ab24995bd40a7e2fd9
viviel <viviel@163.com>
2021-06-10 11:09:21 +0800

    MajorityElement divide

--- algorithm/alg-leetcode/src/main/java/pers/algorithm/leetcode/divide/MajorityElement.java
+++ algorithm/alg-leetcode/src/main/java/pers/algorithm/leetcode/divide/MajorityElement.java
@@ -1,4 +1,4 @@
-package pers.algorithm.leetcode.bit;
+package pers.algorithm.leetcode.divide;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -57,5 +57,43 @@ class MajorityElement {
             }
         }
         return result;
+    }
+
+    public int majorityElement3(int[] nums) {
+        return majorityElementRec(nums, 0, nums.length - 1);
+    }
+
+    private int majorityElementRec(int[] nums, int lo, int hi) {
+        // base case; the only element in an array of size 1 is the majority
+        // element.
+        if (lo == hi) {
+            return nums[lo];
+        }
+
+        // recurse on left and right halves of this slice.
+        int mid = (hi - lo) / 2 + lo;
+        int left = majorityElementRec(nums, lo, mid);
+        int right = majorityElementRec(nums, mid + 1, hi);
+
+        // if the two halves agree on the majority element, return it.
+        if (left == right) {
+            return left;
+        }
+
+        // otherwise, count each element and return the "winner".
+        int leftCount = countInRange(nums, left, lo, hi);
+        int rightCount = countInRange(nums, right, lo, hi);
+
+        return leftCount > rightCount ? left : right;
+    }
+
+    private int countInRange(int[] nums, int num, int lo, int hi) {
+        int count = 0;
+        for (int i = lo; i <= hi; i++) {
+            if (nums[i] == num) {
+                count++;
+            }
+        }
+        return count;
     }
 }
