5ec322ca7e3e5f350293bf0dc46f893dfeeef1db
viviel <viviel@163.com>
2021-06-16 11:42:19 +0800

    MaxProfit

---
+++ algorithm/alg-leetcode/src/main/java/pers/algorithm/leetcode/dynamic/MaxProfit.java
@@ -0,0 +1,54 @@
+package pers.algorithm.leetcode.dynamic;
+
+/**
+ * 给定一个数组 prices ，它的第i 个元素prices[i] 表示一支给定股票第 i 天的价格。
+ * <p>
+ * 你只能选择 某一天 买入这只股票，并选择在 未来的某一个不同的日子 卖出该股票。设计一个算法来计算你所能获取的最大利润。
+ * <p>
+ * 返回你可以从这笔交易中获取的最大利润。如果你不能获取任何利润，返回 0 。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：[7,1,5,3,6,4]
+ * 输出：5
+ * 解释：在第 2 天（股票价格 = 1）的时候买入，在第 5 天（股票价格 = 6）的时候卖出，最大利润 = 6-1 = 5 。
+ * 注意利润不能是 7-1 = 6, 因为卖出价格需要大于买入价格；同时，你不能在买入前卖出股票。
+ * 示例 2：
+ * <p>
+ * 输入：prices = [7,6,4,3,1]
+ * 输出：0
+ * 解释：在这种情况下, 没有交易完成, 所以最大利润为 0。
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= prices.length <= 10^5
+ * 0 <= prices[i] <= 10^4
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+class MaxProfit {
+
+    /**
+     * 尝试使用动态规划
+     * <p>
+     * 前i天的最大收益 = max(前i - 1天的最大收益，第i天的价格 - 前i - 1天中的最小价格)
+     * f(n) = max(f(n - 1), a[n - 1] - min)
+     */
+    public int maxProfit(int[] prices) {
+        if (prices.length <= 1) {
+            return 0;
+        }
+        int max = 0;
+        int min = prices[0];
+        for (int i = 1; i < prices.length; i++) {
+            max = Math.max(max, prices[i] - min);
+            min = Math.min(min, prices[i]);
+        }
+        return max;
+    }
+}
