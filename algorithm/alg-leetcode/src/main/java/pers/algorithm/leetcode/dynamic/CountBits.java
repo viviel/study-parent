bee714d71b5da4d3a1967f1761bf30b421d82ce6
viviel <viviel@163.com>
2021-06-21 11:34:27 +0800

    CountBits

---
+++ algorithm/alg-leetcode/src/main/java/pers/algorithm/leetcode/dynamic/CountBits.java
@@ -0,0 +1,55 @@
+package pers.algorithm.leetcode.dynamic;
+
+/**
+ * 给定一个非负整数num。对于0 ≤ i ≤ num 范围中的每个数字i，计算其二进制数中的 1 的数目并将它们作为数组返回。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: 2
+ * 输出: [0,1,1]
+ * 示例2:
+ * <p>
+ * 输入: 5
+ * 输出: [0,1,1,2,1,2]
+ * 进阶:
+ * <p>
+ * 给出时间复杂度为O(n*sizeof(integer))的解答非常容易。但你可以在线性时间O(n)内用一趟扫描做到吗？
+ * 要求算法的空间复杂度为O(n)。
+ * 你能进一步完善解法吗？要求在C++或任何其他语言中不使用任何内置函数（如 C++ 中的__builtin_popcount）来执行此操作。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/counting-bits
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+class CountBits {
+
+    /**
+     * https://leetcode-cn.com/problems/counting-bits/solution/bi-te-wei-ji-shu-by-leetcode-solution-0t1i/
+     * <p>
+     * 动态规划
+     * <p>
+     * 0  0      0
+     * 1  1      1
+     * 2  10     1
+     * 3  11     2
+     * 4  100    1
+     * 5  101    2
+     * 6  110    2
+     * 7  111    3
+     * 8  1000   1
+     * <p>
+     * f(n) = f(m) + 1
+     * 0 <= m < n
+     */
+    public int[] countBits(int n) {
+        int[] bits = new int[n + 1];
+        int highBit = 0;
+        for (int i = 1; i <= n; i++) {
+            if ((i & (i - 1)) == 0) {
+                highBit = i;
+            }
+            bits[i] = bits[i - highBit] + 1;
+        }
+        return bits;
+    }
+}
