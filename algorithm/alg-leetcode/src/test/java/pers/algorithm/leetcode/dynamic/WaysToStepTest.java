4031f5c5776fd09fd21f4c26bc7fe865bd584b54
viviel <viviel@163.com>
2021-06-24 11:24:31 +0800

    WaysToStep

---
+++ algorithm/alg-leetcode/src/test/java/pers/algorithm/leetcode/dynamic/WaysToStepTest.java
@@ -0,0 +1,28 @@
+package pers.algorithm.leetcode.dynamic;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WaysToStepTest {
+
+    private WaysToStep o;
+
+    @BeforeEach
+    void setUp() {
+        o = new WaysToStep();
+    }
+
+    @Test
+    void test1() {
+        int r = o.waysToStep(3);
+        assertEquals(4, r);
+    }
+
+    @Test
+    void test2() {
+        int r = o.waysToStep(61);
+        assertEquals(752119970, r);
+    }
+}
