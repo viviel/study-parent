f830301b209f217921d42cd5d498ee6070b81d46
viviel <viviel@163.com>
2021-11-01 14:17:57 +0800

    GenerateTrees

---
+++ algorithm/alg-leetcode/src/main/java/pers/algorithm/leetcode/recall/GenerateTrees.java
@@ -0,0 +1,40 @@
+package pers.algorithm.leetcode.recall;
+
+import pers.algorithm.leetcode.tree.common.TreeNode;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * https://leetcode-cn.com/problems/unique-binary-search-trees-ii/
+ */
+class GenerateTrees {
+
+    public List<TreeNode> generateTrees(int n) {
+        if (n == 0) {
+            return new LinkedList<>();
+        }
+        return generateTrees(1, n);
+    }
+
+    public List<TreeNode> generateTrees(int start, int end) {
+        List<TreeNode> allTrees = new LinkedList<>();
+        if (start > end) {
+            allTrees.add(null);
+            return allTrees;
+        }
+        for (int i = start; i <= end; i++) {
+            List<TreeNode> leftTrees = generateTrees(start, i - 1);
+            List<TreeNode> rightTrees = generateTrees(i + 1, end);
+            for (TreeNode left : leftTrees) {
+                for (TreeNode right : rightTrees) {
+                    TreeNode currTree = new TreeNode(i);
+                    currTree.left = left;
+                    currTree.right = right;
+                    allTrees.add(currTree);
+                }
+            }
+        }
+        return allTrees;
+    }
+}
---
+++ algorithm/alg-leetcode/src/test/java/pers/algorithm/leetcode/recall/GenerateTreesTest.java
@@ -0,0 +1,7 @@
+package pers.algorithm.leetcode.recall;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GenerateTreesTest {
+
+}
