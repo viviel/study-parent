254086a2f4204f689fcf1ffe694acd995d60aa55
viviel <viviel@163.com>
2021-11-09 11:15:23 +0800

    Rob

---
+++ algorithm/alg-leetcode/src/test/java/pers/algorithm/leetcode/dynamic/RobTest.java
@@ -0,0 +1,51 @@
+package pers.algorithm.leetcode.dynamic;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class RobTest {
+
+    private Rob o;
+
+    @BeforeEach
+    void setUp() {
+        o = new Rob();
+    }
+
+    /**
+     * 1,2,3,1
+     */
+    @Test
+    void t1() {
+        int[] nums = new int[]{1, 2, 3, 1};
+        int ans = o.rob(nums);
+        assertEquals(4, ans);
+    }
+
+    /**
+     * 2,7,9,3,1
+     *
+     * 2
+     * 2
+     *
+     * 2 7
+     * 7
+     *
+     * 2 7 9
+     * 11
+     *
+     * 2 7 9 3
+     * 11
+     *
+     * 2 7 9 3 1
+     * 12
+     */
+    @Test
+    void t2() {
+        int[] nums = new int[]{2, 7, 9, 3, 1};
+        int ans = o.rob(nums);
+        assertEquals(12, ans);
+    }
+}
