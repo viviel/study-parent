c030432a4c26d46ae925bb02a2ed9b80a38f6df0
viviel <viviel@163.com>
2021-11-15 16:44:19 +0800

    ReverseKGroup

---
+++ algorithm/alg-leetcode/src/main/java/pers/algorithm/leetcode/recursion/ReverseKGroup.java
@@ -0,0 +1,46 @@
+package pers.algorithm.leetcode.recursion;
+
+import pers.algorithm.leetcode.list.ListNode;
+
+/**
+ * https://leetcode-cn.com/problems/reverse-nodes-in-k-group/
+ * 确定思路
+ *  初始化快指针边界
+ *
+ * 解题步骤
+ * 再次梳理思路和步骤
+ * 测试
+ */
+class ReverseKGroup {
+
+    private ListNode bound;
+
+    public ListNode reverseKGroup(ListNode head, int k) {
+        bound = head;
+        for (int i = 0; i < k - 1 && bound != null; i++) {
+            bound = bound.next;
+        }
+        return traversal(null, head, k);
+    }
+
+    private ListNode traversal(ListNode pre, ListNode cur, int k) {
+        if (bound == null) {
+            return cur;
+        }
+        if (cur == null) {
+            return null;
+        }
+        ListNode tail = cur;
+        for (int i = 0; i < k && cur != null; i++) {
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+            if (bound != null) {
+                bound = bound.next;
+            }
+        }
+        tail.next = traversal(pre, cur, k);
+        return pre;
+    }
+}
---
+++ algorithm/alg-leetcode/src/test/java/pers/algorithm/leetcode/recursion/ReverseKGroupTest.java
@@ -0,0 +1,35 @@
+package pers.algorithm.leetcode.recursion;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import pers.algorithm.leetcode.list.ListNode;
+import pers.algorithm.leetcode.list.ListNodes;
+
+class ReverseKGroupTest {
+
+    private ReverseKGroup o;
+
+    @BeforeEach
+    void setUp() {
+        o = new ReverseKGroup();
+    }
+
+    /**
+     * 0 -> 1 -> 2 -> 3 -> 4 -> 5
+     * 0 -> 1    2 -> 3 -> 4 -> 5
+     * 0 -> 1 <- 2    3 -> 4 -> 5
+     *
+     * 0 -> 1 <- 2 <- 3 <- 4    5
+     */
+    @Test
+    void t1() {
+        ListNode listNode = ListNodes.listNode(5);
+        ListNode ans = o.reverseKGroup(listNode, 3);
+    }
+
+    @Test
+    void t2() {
+        ListNode listNode = ListNodes.listNode(5);
+        ListNode ans = o.reverseKGroup(listNode, 2);
+    }
+}
