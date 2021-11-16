7210182898567ae4e447fa3b1afe2a23fb795300
viviel <viviel@163.com>
2021-11-16 12:01:40 +0800

    LRUCache

---
+++ algorithm/alg-leetcode/src/main/java/pers/algorithm/leetcode/design/LRUCache.java
@@ -0,0 +1,80 @@
+package pers.algorithm.leetcode.design;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * 运用你所掌握的数据结构，设计和实现一个 LRU (最近最少使用) 缓存机制 。
+ * 实现 LRUCache 类：
+ * <p>
+ * LRUCache(int capacity) 以正整数作为容量capacity 初始化 LRU 缓存
+ * int get(int key) 如果关键字 key 存在于缓存中，则返回关键字的值，否则返回 -1 。
+ * void put(int key, int value)如果关键字已经存在，则变更其数据值；如果关键字不存在，则插入该组「关键字-值」。
+ * 当缓存容量达到上限时，它应该在写入新数据之前删除最久未使用的数据值，从而为新的数据值留出空间。
+ * <p>
+ * <p>
+ * 进阶：你是否可以在O(1) 时间复杂度内完成这两种操作？
+ * <p>
+ * <p>
+ * <p>
+ * 示例：
+ * <p>
+ * 输入
+ * ["LRUCache", "put", "put", "get", "put", "get", "put", "get", "get", "get"]
+ * [[2], [1, 1], [2, 2], [1], [3, 3], [2], [4, 4], [1], [3], [4]]
+ * 输出
+ * [null, null, null, 1, null, -1, null, -1, 3, 4]
+ * <p>
+ * 解释
+ * LRUCache lRUCache = new LRUCache(2);
+ * lRUCache.put(1, 1); // 缓存是 {1=1}
+ * lRUCache.put(2, 2); // 缓存是 {1=1, 2=2}
+ * lRUCache.get(1);    // 返回 1
+ * lRUCache.put(3, 3); // 该操作会使得关键字 2 作废，缓存是 {1=1, 3=3}
+ * lRUCache.get(2);    // 返回 -1 (未找到)
+ * lRUCache.put(4, 4); // 该操作会使得关键字 1 作废，缓存是 {4=4, 3=3}
+ * lRUCache.get(1);    // 返回 -1 (未找到)
+ * lRUCache.get(3);    // 返回 3
+ * lRUCache.get(4);    // 返回 4
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= capacity <= 3000
+ * 0 <= key <= 10000
+ * 0 <= value <= 10^5
+ * 最多调用 2 * 10^5 次 get 和 put
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/lru-cache
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+class LRUCache extends LinkedHashMap<Integer, Integer> {
+
+    private final int capacity;
+
+    public LRUCache(int capacity) {
+        super(capacity, 0.75F, true);
+        this.capacity = capacity;
+    }
+
+    public int get(int key) {
+        return super.getOrDefault(key, -1);
+    }
+
+    public void put(int key, int value) {
+        super.put(key, value);
+    }
+
+    @Override
+    protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
+        return size() > capacity;
+    }
+}
+
+/*
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
---
+++ algorithm/alg-leetcode/src/test/java/pers/algorithm/leetcode/design/LRUCacheTest.java
@@ -0,0 +1,47 @@
+package pers.algorithm.leetcode.design;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.PriorityQueue;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LRUCacheTest {
+
+    private LRUCache o;
+
+    @BeforeEach
+    void setUp() {
+        o = new LRUCache(2);
+    }
+
+    @Test
+    void t1() {
+        o.put(1, 1);
+        o.put(2, 2);
+        int i = o.get(1);
+        assertEquals(1, i);
+        o.put(3, 3);
+        int i1 = o.get(2);
+        assertEquals(-1, i1);
+        o.put(4, 4);
+        int i2 = o.get(1);
+        assertEquals(-1, i2);
+        int i3 = o.get(3);
+        assertEquals(3, i3);
+        int i4 = o.get(4);
+        assertEquals(4, i4);
+    }
+
+    @Test
+    void t2() {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        pq.add(2);
+        pq.add(1);
+        pq.add(3);
+        while (!pq.isEmpty()) {
+            System.out.println(pq.poll());
+        }
+    }
+}
